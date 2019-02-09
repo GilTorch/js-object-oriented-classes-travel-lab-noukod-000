e801b5507821c344d89169d59f6e4ad29f949de0
Gilbert Torchon <gilbert@noukod.com>
2019-02-09 14:06:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+class Driver{
+  constructor(name,startDate){
+    this.name=name;
+    this.startDate=new Date(startDate)
+  }
 
+  yearsExperienceFromBeginningOf(endYear){
+    return (endYear-this.startDate.getFullYear())-1;
+  }
+}
+
+
+class Route{
+  blocksTravelled(){
+
+  }
+}
